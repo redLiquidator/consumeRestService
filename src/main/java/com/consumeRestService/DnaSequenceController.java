f7c9faebea23043cf617baf164b9fd1e61cfff9c
redLiquidator <inufuni@naver.com>
2021-08-11 08:02:01 +0900

    dnasequence

---
+++ src/main/java/com/consumeRestService/DnaSequenceController.java
@@ -0,0 +1,39 @@
+package com.consumeRestService;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.contractdto.DnaSequence;
+
+@RestController
+@RequestMapping(DnaSequenceController.DNASEQUENCE_BASE_URI)
+public class DnaSequenceController {
+public static final String DNASEQUENCE_BASE_URI = "dna/v1/dnaSequence";
+	
+	@RequestMapping(value="{dnaLength}")
+	public DnaSequence getDnaSequenceByLength(@PathVariable final int dnaLength) {
+		DnaSequence dnaSequence = new DnaSequence ();
+		//create random strand with its length 100 bases
+		String seq = "";
+			for(int i = 0; i < dnaLength; i++) {
+				    double rVal = Math.random()*4;
+				    if (rVal<1) {
+				    	seq = seq+ 'G';
+				    }else if(1<=rVal| rVal<2) {
+				    	seq = seq+ 'C';
+				    }else if(2<=rVal| rVal<3) {
+				    	seq = seq+ 'A';
+				    }else {
+				    	seq = seq+ 'T';
+				    }
+				}
+		System.out.println("seq "+seq);
+		dnaSequence.setSequence(seq);
+		dnaSequence.setCreationDate(new Date());
+		return dnaSequence;
+	} 
+
+}
